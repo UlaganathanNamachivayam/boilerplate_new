6a2653bdbc7ef96c29414ee1fa884b40cda7293e
UlaganathanNamachivayam <ulags.n@gmail.com>
2018-08-14 11:36:58 +0530

    Added nginx to sample shell script.

--- Sample.sh
+++ Sample.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
-echo "This is for testing second time" >> /etc/config
-yum -y install nginx 
-echo "Hello Playground" >> /var/www/html/index.html
-service nginx restart
+
+apk -y update
+apk -y add nginx
+adduser -D -g 'www' www
+mkdir /www
+chown -R www:www /var/lib/nginx
+chown -R www:www /www
+
+cat <<EOT >> /etc/nginx/nginx.conf
+user                            www;
+worker_processes                auto; # it will be determinate automatically by the number of core
+
+error_log                       /var/log/nginx/error.log warn;
+#pid                             /var/run/nginx.pid; # it permit you to use /etc/init.d/nginx reload|restart|stop|start
+
+events {
+    worker_connections          1024;
+}
+
+http {
+    include                     /etc/nginx/mime.types;
+    default_type                application/octet-stream;
+    sendfile                    on;
+    access_log                  /var/log/nginx/access.log;
+    keepalive_timeout           3000;
+    server {
+        listen                  80;
+        root                    /www;
+        index                   index.html index.htm;
+        server_name             localhost;
+        client_max_body_size    32m;
+        error_page              500 502 503 504  /50x.html;
+        location = /50x.html {
+              root              /var/lib/nginx/html;
+        }
+    }
+}
+EOT
+
+cat << EOT >> /www/index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>HTML5</title>
+</head>
+<body>
+    Hello Playground
+</body>
+</html>
+EOT
+
+rc-service nginx start
+rc-service nginx restart
